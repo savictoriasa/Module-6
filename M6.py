3b935a7c0c9bd8339a7e4d8bdb3d88ab73d94869
savictoriasa <137753833+savictoriasa@users.noreply.github.com>
2023-06-26 01:59:53 -0400

    Update M6.py

--- M6.py
+++ M6.py
@@ -1,5 +1,5 @@
-'''
-#CURRENT TIME FUNCTIONING CODE
+#EXERCISE 1
+#CURRENT TIME CODE THAT I UNDERSTOOD
 from datetime import datetime
 
 now = datetime.now()
@@ -7,7 +7,8 @@ now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
 
-#PROFESSOR'S CODE
+
+#TRYING TO MAKE PROFESSOR'S CODE WORK
 import sys
 from datetime import datetime
 from datetime import time
@@ -25,7 +26,21 @@ print("{dt}\t{time_string}\t{store}\t{item}\t{cost}\t{payment}")
 
 main()
 
-#EXERCISE #3 WORKING
+
+#EXERCISE 2
+from datetime import datetime, timedelta
+
+finalt = (datetime.now() - timedelta(seconds = 60)).strftime('%Y:%H:%M:%S')
+print("Time minus 60 seconds: " + finalt)
+
+finaly = (datetime.now() + timedelta(hours = 17520)).strftime('%Y:%H:%M:%S')
+print("Time plus 2 years: " + finaly)
+
+endtime = (datetime.now() + timedelta(hours = 17520) - timedelta(seconds = 60)).strftime('%Y:%H:%M:%S')
+print("Final time minus 60 seconds and plus 2 years: " + endtime)
+
+
+#EXERCISE 3 
 from datetime import timedelta
 
 d = timedelta(microseconds=-1)
@@ -34,13 +49,17 @@ d = timedelta(microseconds=-1)
 
 d = timedelta(days = 100, hours = 10, minutes = 13)
 print(d)
-'''
 
 
+#EXERCISE 4
+#GIVEN SCRIPT
+from datetime import datetime 
+# get current date 
+datetime_object = datetime.now() 
+print(datetime_object) 
+print('Type :- ',type(datetime_object)) 
 
-
-
-
+#CREATING SEPARATE FUNCTION
 from datetime import datetime 
 # get current date
 def function(feet, inches):
@@ -51,3 +70,13 @@ def function(feet, inches):
      print('Type :- ', (datetime_object))
      function(feet,inches)
 
+#CREATING DATETIME_OBJECT FUNCTION
+from datetime import datetime
+# get current date
+def datetime_object(feet, inches):
+    feet = 1
+    inches = 12
+    datetime_object = datetime.now()
+    print(datetime_object)
+    print('Type :- ',type(datetime_object))
+    datetime_object(feet, inches)
